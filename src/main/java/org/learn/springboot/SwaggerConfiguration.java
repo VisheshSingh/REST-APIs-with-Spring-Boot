854939f6157063345d22533da645f6660f0495df
Vishesh <visheshsingh16@gmail.com>
2018-12-27 22:46:29 -0500

    APIInfo added to Swagger documentation

--- src/main/java/org/learn/springboot/SwaggerConfiguration.java
+++ src/main/java/org/learn/springboot/SwaggerConfiguration.java
@@ -1,10 +1,14 @@
 package org.learn.springboot;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,7 +24,19 @@ public class SwaggerConfiguration {
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("org.learn.springboot"))
 				.paths(PathSelectors.regex("/api.*"))
-				.build();
+				.build()
+				.apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfo("REST API - Books resource",
+				"Spring Boot based REST API",
+				"1.0.0",
+				"For terms and conditions visit the docs",
+				new Contact("V-boy", "v-boy-springboot.org", "v-boy@springboot.org"),
+				"GNU",
+				"http://www.gnu.org",
+				Collections.emptyList());
 	}
 
 }
