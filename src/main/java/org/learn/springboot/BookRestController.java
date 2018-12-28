a9a5ef3fb82388f123b0cadb276077813072b49d
Vishesh <visheshsingh16@gmail.com>
2018-12-27 20:17:39 -0500

    Added Put and Delete functionality

--- src/main/java/org/learn/springboot/BookRestController.java
+++ src/main/java/org/learn/springboot/BookRestController.java
@@ -1,14 +1,16 @@
 package org.learn.springboot;
 
 import java.util.Map;
-import java.util.List;
+import java.util.Collection;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class BookRestController {
 	
-	List<Book> books = BookRepository.BOOKS;
 	Map<Integer, Book> BOOKMAP = BookRepository.BOOKMAP;
 	
 	@GetMapping(value="/books", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<List<Book>> getAllBooks() {
+	public ResponseEntity<Collection<Book>> getAllBooks() {
 		
-		return new ResponseEntity<List<Book>>(books, HttpStatus.OK);
+		return new ResponseEntity<Collection<Book>>(BOOKMAP.values(), HttpStatus.OK);
 	}
 	
 	@GetMapping(value="/books/{id}", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -37,8 +38,29 @@ public class BookRestController {
 	@PostMapping(value="/books", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<Void> addBook(@RequestBody Book book) {
 		
-		books.add(book);
 		BOOKMAP.put(book.getId(), book);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+		
+	}
+	
+	@PutMapping(value="/books", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<Book> editBook(@RequestBody Book book) {
+		
+		if(BOOKMAP.containsKey(book.getId())) {
+			BOOKMAP.replace(book.getId(), book);
+		} else {
+			addBook(book);
+		}
+		return new ResponseEntity<Book>(book, HttpStatus.OK);
+		
+	}
+	
+	@DeleteMapping(value="/books/{id}", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<Void> deleteBook(@PathVariable Integer id) {
+		
+		if(BOOKMAP.containsKey(id)) {
+			BOOKMAP.remove(id);
+		}
 		return new ResponseEntity<Void>(HttpStatus.OK);
 		
 	}
